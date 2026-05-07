ed5b86450e7c7e2881fbbe8400248509ea288220
Ssantiagolopezz <162466422+Ssantiagolopezz@users.noreply.github.com>
2026-05-06 21:02:06 -0300

    Add files via upload

---
+++ Prueba tecnica/frontend/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// Importamos las vistas (las crearemos en el siguiente paso)
+import ClienteView from '../views/ClienteView.vue'
+import LogisticaView from '../views/LogisticaView.vue'
+import AdminView from '../views/AdminView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'cliente',
+      component: ClienteView
+    },
+    {
+      path: '/logistica',
+      name: 'logistica',
+      component: LogisticaView
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: AdminView
+    }
+  ]
+})
+
+export default router
